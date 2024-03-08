38420680f94897515d5f85480bd5cd5b33c0f324
Joshua Flores <142363341+git-yehoshua@users.noreply.github.com>
2024-03-09 01:06:48 +0800

    route testing new element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import SignIn from "./pages/users/signin-page";
 import SignUp from "./pages/users/signup-page";
 import Dashboard from "./pages/admin/dashboard-page";
 import LandingPage from "./pages/users/landing-page";
+import Home from "./pages/for-testing/home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </>
   );
