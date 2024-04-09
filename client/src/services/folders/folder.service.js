12c78e8001e810a2cafb2fd2f97a894a244a700b
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-04-09 11:32:41 +0800

    edit prettier settings

--- client/src/services/folders/folder.service.js
+++ client/src/services/folders/folder.service.js
@@ -1,42 +1,51 @@
-import { addDoc, collection, deleteDoc, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
-import { db } from "../../database/firebase-connection";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore'
+import { db } from '../../database/firebase-connection'
 
 // get the folders
 export const fetchFolders = async (parentId = null) => {
-  const q = query(collection(db, "folders"), where("parentId", "==", parentId));
-  const querySnapshot = await getDocs(q);
+  const q = query(collection(db, 'folders'), where('parentId', '==', parentId))
+  const querySnapshot = await getDocs(q)
   return querySnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
     subfolders: [],
-  }));
-};
+  }))
+}
 
 // add folders
 export const addFolder = async (folderData) => {
-  const docRef = await addDoc(collection(db, "folders"), folderData);
-  return { id: docRef.id, ...folderData, subfolders: [] };
-};
+  const docRef = await addDoc(collection(db, 'folders'), folderData)
+  return { id: docRef.id, ...folderData, subfolders: [] }
+}
 
 // delete folders
 export const deleteFolder = async (folderId) => {
-  await deleteDoc(doc(db, "folders", folderId));
-};
+  await deleteDoc(doc(db, 'folders', folderId))
+}
 
 // update folders name
 export const updateFolderName = async (folderId, newName) => {
-  const folderRef = doc(db, "folders", folderId);
-  await updateDoc(folderRef, { name: newName });
-};
+  const folderRef = doc(db, 'folders', folderId)
+  await updateDoc(folderRef, { name: newName })
+}
 
 // get the folder details
 export const fetchFolderDetails = async (folderId) => {
-  const folderDocRef = doc(db, "folders", folderId);
-  const folderDoc = await getDoc(folderDocRef);
+  const folderDocRef = doc(db, 'folders', folderId)
+  const folderDoc = await getDoc(folderDocRef)
   if (folderDoc.exists()) {
-    return { id: folderDoc.id, ...folderDoc.data() };
+    return { id: folderDoc.id, ...folderDoc.data() }
   } else {
-    console.log("No such folder!");
-    return null;
+    console.log('No such folder!')
+    return null
   }
-};
+}
