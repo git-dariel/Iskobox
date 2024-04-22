2abc6367d44a6b4091685d465737d350e950be6d
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-04-23 01:32:42 +0800

    update auth page

--- client/src/services/users/user-service.js
+++ client/src/services/users/user-service.js
@@ -1,45 +1,43 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { doc, setDoc, collection, query, where, getDocs } from "firebase/firestore";
-import { db } from "../../database/firebase-connection";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../database/firebase-connection';
 
 // register a user
-export async function registerUser(email, password, firstname, lastname, idNumber, role) {
-    try {
-        const auth = getAuth();
-        const usersRef = collection(db, "users");
-        const q = query(usersRef, where("email", "==", email));
-        const querySnapshot = await getDocs(q);
+export async function registerUser(email, password, firstname, lastname) {
+  try {
+    const auth = getAuth();
+    const usersRef = collection(db, 'users');
+    const q = query(usersRef, where('email', '==', email));
+    const querySnapshot = await getDocs(q);
 
-        if (!querySnapshot.empty) {
-            console.log("User already exists!");
-            return false
-        }
-
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const userRef = doc(db, "users", userCredential.user.uid);
-        await setDoc(userRef, {
-            firstname,
-            lastname,
-            idNumber,
-            role,
-            email,
-        })
-
-        return true;
-    } catch (error) {
-        console.error(error);
-        throw error;
+    if (!querySnapshot.empty) {
+      console.log('User already exists!');
+      return false;
     }
+
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const userRef = doc(db, 'users', userCredential.user.uid);
+    await setDoc(userRef, {
+      firstname,
+      lastname,
+      email,
+    });
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 // login a user
 export async function loginUser(email, password) {
-    try {
-        const auth = getAuth();
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        return userCredential.user;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+  try {
+    const auth = getAuth();
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
