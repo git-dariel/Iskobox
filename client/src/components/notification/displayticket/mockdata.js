34b75ceb0536d8adcff48f72d2c235156c0242b2
CodeGaia <jasonnazzerocampo@gmail.com>
2024-05-03 02:27:05 +0800

    updated

---
+++ client/src/components/notification/displayticket/mockdata.js
@@ -0,0 +1,24 @@
+
+
+const mockedData = {
+    notificationData: [
+        {
+            title: "Johnson baby",
+            description: "Requesting to open folder.",
+          },
+          {
+            title: "Mang kanowr",
+            description: "Requesting to open folder",
+          },
+          {
+            title: "Lexy Toys",
+            description: "Requesting Shit.",
+          },
+          {
+            title: "Bob Ong",
+            description: "Requesting to open folder",
+          },
+    ]
+  };
+  
+  export default mockedData;
