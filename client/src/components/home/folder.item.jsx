67295c1b77e0f50ebe63da39abdc7f001860e926
Joshua Flores <142363341+git-yehoshua@users.noreply.github.com>
2024-05-17 21:22:24 +0800

    chore: Update folder.item component to use FcFolder icon and improve styling

--- client/src/components/home/folder.item.jsx
+++ client/src/components/home/folder.item.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { RiFolderLine, RiFolderOpenLine } from 'react-icons/ri';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FcFolder } from "react-icons/fc";
 
 const FolderItem = ({ folder, isGridView, onDoubleClick }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   const handleDoubleClick = () => {
-    setIsOpen(!isOpen);
     onDoubleClick(folder.id);
   };
 
@@ -16,18 +13,14 @@ const FolderItem = ({ folder, isGridView, onDoubleClick }) => {
   return (
     <Link to={folderUrl}>
       <div
-        className={`cursor-default ${
-          isGridView ? 'flex-col m-2 p-2 border' : 'w-full border-y'
+        className={`cursor-default px-1 py-2  ${
+          isGridView ? "flex-col m-2 p-2 border" : "w-full border-b"
         } text-sm flex items-center space-x-2 border-gray-200 hover:bg-gray-100`}
         onDoubleClick={handleDoubleClick}
       >
-        {isOpen ? (
-          <RiFolderOpenLine className='h-8 w-8 text-gray-600' />
-        ) : (
-          <RiFolderLine className='h-8 w-8 text-gray-600' />
-        )}
-        <span className='truncate'>{folder.name}</span>
-        <span className='ml-auto'>{displayLimit}</span>
+        <FcFolder size={30} />
+        <span className="truncate">{folder.name}</span>
+        <span className="ml-auto">{displayLimit}</span>
       </div>
     </Link>
   );
--- client/src/pages/admin/dashboard-page.jsx
+++ client/src/pages/admin/dashboard-page.jsx
@@ -4,11 +4,13 @@ import mockedConfig from "@/configs/mocked.config";
 
 function DashboardContent() {
   return (
-    <div className="flex h-screen bg-gray-200">
+    <div className="flex h-screen bg-[#f8fafd] flex-1">
       {/* Cards */}
-      <section className="m-2">
+      <section className="m-2 w-[70%]">
         <Cards data={mockedConfig.dashboardCardData} />
+        <section className="bg-gray-200">sdff</section>
       </section>
+      <section className="bg-gray-200 w-[30%]">lksdjfl</section>
     </div>
   );
 }
