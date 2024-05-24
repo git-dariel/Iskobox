c132cc89fe60aa91729333c4a0a9190ec8f3eba5
CodeGaia <jasonnazzerocampo@gmail.com>
2024-05-24 13:19:41 +0800

    sdasd

--- client/src/pages/workspace.jsx
+++ client/src/pages/workspace.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 import SideMenu from "@/components/layout/side-menu";
 import TopNavigation from "@/components/layout/top-nav";
-import TicketModal from "@/components/notification/addticket/ticketmodal";
-import CommentButton from "@/components/comment/commentbutton";
-// import CommentList from "@/components/comment/showcomment";
 
-
+import TicketForm from "@/components/notification/addticket/ticket";
 
 function Workspace() {
   return (
     <div className="flex h-screen bg-[#f8fafd]">
       <SideMenu />
-      <div className="flex flex-col flex-1 ">
-        <TopNavigation />
-        <div className="flex flex-col flex-1 p-4 overflow-y-auto items-center justify-center">
-          {/* Main Content */}
-      
-        <TicketModal/>
-        <CommentButton/>
-        {/* <CommentList/> */}
-        
-          {/* <h1>Workspace component/content here</h1> */}
+      <div className="flex flex-col flex-1">
+
+          <TopNavigation />
+       
+        <div className="h-full flex  items-center justify-center">
+          <div className=" ">
+            <TicketForm />
+          </div>
         </div>
       </div>
     </div>
