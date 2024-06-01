92de0bc3bcd11d73c7c7ec48de125fe6e13ad73b
Joshua Flores <142363341+git-yehoshua@users.noreply.github.com>
2024-06-02 01:31:52 +0800

    feat: Add lock feature to FileView component

--- client/src/components/home/folder.item.jsx
+++ client/src/components/home/folder.item.jsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FcFolder } from 'react-icons/fc';
-import CircleButton from '../common/buttons/reusable/circle.button';
-import { deleteFolder } from '../../services/folders/folder.service';
-import { Toaster, toast } from 'sonner';
-import { useUpdate } from '@/helpers/update.context';
-import { bouncy } from 'ldrs';
-import { MdDelete } from 'react-icons/md';
-import { FaUserAlt, FaFileAlt } from 'react-icons/fa';
-import { FiEdit } from 'react-icons/fi';
-import UpdateFolderForm from '../modals/update.folder';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FcFolder } from "react-icons/fc";
+import CircleButton from "../common/buttons/reusable/circle.button";
+import { deleteFolder } from "../../services/folders/folder.service";
+import { Toaster, toast } from "sonner";
+import { useUpdate } from "@/helpers/update.context";
+import { bouncy } from "ldrs";
+import { MdDelete } from "react-icons/md";
+import { FaUserAlt, FaFileAlt } from "react-icons/fa";
+import { FiEdit } from "react-icons/fi";
+import UpdateFolderForm from "../modals/update.folder";
 
 const FolderItem = ({ folder, onDoubleClick, isGridView }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalOpenEdit, setIsModalOpenEdit] = useState(false);
   const { triggerUpdate } = useUpdate();
   const [loading, setLoading] = useState(false);
+  const [isDueDateMet, setIsDueDateMet] = useState(false);
   bouncy.register();
 
+  useEffect(() => {
+    // Check if the due date has passed
+    const checkDueDate = () => {
+      const dueDate = new Date(folder.dueDate);
+      const currentDate = new Date();
+      setIsDueDateMet(currentDate >= dueDate);
+    };
+    checkDueDate();
+  }, [folder.dueDate]);
+
   const handleDoubleClick = () => {
-    onDoubleClick(folder.id);
+    if (isDueDateMet) {
+      toast.error("Folder disabled, send a ticket to admin", {
+        position: "top-center",
+      });
+    } else {
+      onDoubleClick(folder.id);
+    }
   };
 
   const toggleModal = () => {
-    setIsModalOpenEdit(!isModalOpenEdit);
+    if (isDueDateMet) {
+      toast.error("Folder disabled, send a ticket to admin");
+    } else {
+      setIsModalOpenEdit(!isModalOpenEdit);
+    }
   };
 
   const handleDelete = async () => {
     setLoading(true);
     try {
       await deleteFolder(folder.id);
-      toast.success('Folder removed');
+      toast.success("Folder removed");
       triggerUpdate();
     } catch (error) {
-      console.error('Error deleting folder:', error);
-      toast.error('Failed to remove folder. Please try again.');
+      console.error("Error deleting folder:", error);
+      toast.error("Failed to remove folder. Please try again.");
     } finally {
       setIsModalOpen(false);
       setLoading(false);
@@ -43,7 +64,11 @@ const FolderItem = ({ folder, onDoubleClick, isGridView }) => {
 
   const openModal = (e) => {
     e.preventDefault(); // Prevent link navigation
-    setIsModalOpen(true);
+    if (isDueDateMet) {
+      toast.error("Folder disabled, send a ticket to admin");
+    } else {
+      setIsModalOpen(true);
+    }
   };
 
   const closeModal = () => {
@@ -56,85 +81,127 @@ const FolderItem = ({ folder, onDoubleClick, isGridView }) => {
   return (
     <>
       <Toaster />
-      <Link to={folderUrl}>
-        <div
-          className={`cursor-default p-4 ${
-            isGridView ? 'flex flex-col m-2 p-2 border' : 'flex w-full border-b'
-          } text-sm flex items-center justify-between space-x-4 border-gray-200 hover:bg-gray-100 transition-all duration-150`}
-          onDoubleClick={handleDoubleClick}
+      <div
+        className={`relative ${
+          isDueDateMet ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+      >
+        <Link
+          to={isDueDateMet ? "#" : folderUrl}
+          onClick={
+            isDueDateMet
+              ? (e) => {
+                  e.preventDefault();
+                  toast.error("Folder disabled, send a ticket to admin");
+                }
+              : null
+          }
         >
-          <div className='flex items-center w-full space-x-2'>
-            <FcFolder size={40} />
-            <div className='flex w-full flex-col'>
-              <span className='truncate text-lg font-semibold'>{folder.name}</span>
-              {!isGridView && (
-                <div className='flex items-center w-full space-x-1 text-gray-500 text-xs'>
-                  <FaUserAlt size={12} />
-                  <span>Assignee</span>
+          <div
+            className={`cursor-default p-4 ${
+              isGridView
+                ? "flex flex-col m-2 p-2 border"
+                : "flex w-full border-b"
+            } text-sm flex items-center justify-between space-x-4 border-gray-200 hover:bg-gray-100 transition-all duration-150`}
+            onDoubleClick={handleDoubleClick}
+          >
+            <div className="flex items-center w-full space-x-2">
+              <FcFolder size={40} />
+              <div className="flex w-full flex-col">
+                <span className="truncate text-lg font-semibold">
+                  {folder.name}
+                </span>
+                {!isGridView && (
+                  <div className="flex items-center w-full space-x-1 text-gray-500 text-xs">
+                    <FaUserAlt size={12} />
+                    <span>Assignee</span>
+                  </div>
+                )}
+              </div>
+            </div>
+            {!isGridView && (
+              <div className="ml-auto flex items-center space-x-6">
+                <div className="flex items-center space-x-1 text-gray-500">
+                  <FaFileAlt size={16} />
+                  <span>{displayLimit}</span>
                 </div>
+                <CircleButton
+                  title={"Edit"}
+                  icon={<FiEdit size={20} />}
+                  onClick={toggleModal}
+                  disabled={isDueDateMet}
+                />
+                <CircleButton
+                  title={"Remove"}
+                  icon={<MdDelete size={20} />}
+                  onClick={openModal}
+                  disabled={isDueDateMet}
+                />
+              </div>
+            )}
+            {isGridView && (
+              <div className="ml-auto">
+                <CircleButton
+                  title={"Edit"}
+                  icon={<FiEdit size={20} />}
+                  onClick={toggleModal}
+                  disabled={isDueDateMet}
+                />
+                <CircleButton
+                  title={"Remove"}
+                  icon={<MdDelete size={20} />}
+                  onClick={openModal}
+                  disabled={isDueDateMet}
+                />
+              </div>
+            )}
+          </div>
+        </Link>
+
+        {isModalOpen && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+            <div className="bg-gray-300 p-8 rounded-lg shadow-2xl">
+              <h2 className="text-xl font-semibold mb-6">Delete Folder</h2>
+              {loading ? (
+                <div className="flex justify-center items-center">
+                  <l-bouncy size="40" color="black"></l-bouncy>
+                </div>
+              ) : (
+                <>
+                  <p className="mb-4">
+                    Are you sure you want to delete the folder "{folder.name}"?
+                  </p>
+                  <div className="flex justify-end space-x-4">
+                    <button
+                      onClick={() => setIsModalOpen(false)}
+                      className="px-6 py-2 border bg-gray-50 text-gray-700 rounded-lg hover:bg-gray-100 transition-colors"
+                    >
+                      No
+                    </button>
+                    <button
+                      onClick={handleDelete}
+                      className="px-6 py-2 bg-slate-700 text-white rounded-lg hover:bg-slate-800 transition-colors"
+                    >
+                      Yes
+                    </button>
+                  </div>
+                </>
               )}
             </div>
           </div>
-          {!isGridView && (
-            <div className='ml-auto flex items-center space-x-6'>
-              <div className='flex items-center space-x-1 text-gray-500'>
-                <FaFileAlt size={16} />
-                <span>{displayLimit}</span>
-              </div>
-              <CircleButton title={'Edit'} icon={<FiEdit size={20} />} onClick={toggleModal} />
-              <CircleButton title={'Remove'} icon={<MdDelete size={20} />} onClick={openModal} />
-            </div>
-          )}
-          {isGridView && (
-            <div className='ml-auto'>
-              <CircleButton title={'Edit'} icon={<FiEdit size={20} />} onClick={toggleModal} />
-              <CircleButton title={'Remove'} icon={<MdDelete size={20} />} onClick={openModal} />
-            </div>
-          )}
-        </div>
-      </Link>
-
-      {isModalOpen && (
-        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75'>
-          <div className='bg-gray-300 p-8 rounded-lg shadow-2xl'>
-            <h2 className='text-xl font-semibold mb-6'>Delete Folder</h2>
-            {loading ? (
-              <div className='flex justify-center items-center'>
-                <l-bouncy size='40' color='black'></l-bouncy>
-              </div>
-            ) : (
-              <>
-                <p className='mb-4'>Are you sure you want to delete the folder "{folder.name}"?</p>
-                <div className='flex justify-end space-x-4'>
-                  <button
-                    onClick={() => setIsModalOpen(false)}
-                    className='px-6 py-2 border bg-gray-50 text-gray-700 rounded-lg hover:bg-gray-100 transition-colors'
-                  >
-                    No
-                  </button>
-                  <button
-                    onClick={handleDelete}
-                    className='px-6 py-2 bg-slate-700 text-white rounded-lg hover:bg-slate-800 transition-colors'
-                  >
-                    Yes
-                  </button>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-      )}
-      {isModalOpenEdit && (
-        <UpdateFolderForm
-          onClose={toggleModal}
-          folderDetails={{
-            id: folder.id,
-            name: folder.name,
-            dueDate: folder.dueDate,
-            uploadLimit: folder.uploadLimit,
-          }}
-        />
-      )}
+        )}
+        {isModalOpenEdit && (
+          <UpdateFolderForm
+            onClose={toggleModal}
+            folderDetails={{
+              id: folder.id,
+              name: folder.name,
+              dueDate: folder.dueDate,
+              uploadLimit: folder.uploadLimit,
+            }}
+          />
+        )}
+      </div>
     </>
   );
 };
--- client/src/components/modals/new.folder.jsx
+++ client/src/components/modals/new.folder.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
-import OvalButton from '../common/buttons/reusable/oval.button';
-import { IoClose } from 'react-icons/io5';
-import CircleButton from '../common/buttons/reusable/circle.button';
-import { Toaster, toast } from 'sonner';
+import React, { useEffect, useRef, useState } from "react";
+import OvalButton from "../common/buttons/reusable/oval.button";
+import { IoClose } from "react-icons/io5";
+import CircleButton from "../common/buttons/reusable/circle.button";
+import { Toaster, toast } from "sonner";
 
 const NewFolderForm = ({ onClose, onCreate, parentId = null }) => {
   const modalRef = useRef(null);
-  const [folderName, setFolderName] = useState('');
-  const [dueDate, setDueDate] = useState('');
+  const [folderName, setFolderName] = useState("");
+  const [dueDate, setDueDate] = useState("");
   const [uploadLimit, setUploadLimit] = useState(0);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!folderName || !dueDate || !uploadLimit) {
-      return toast.error('Please fill all fields');
+      return toast.error("Please fill all fields");
     }
     if (onCreate) {
       try {
@@ -28,8 +28,8 @@ const NewFolderForm = ({ onClose, onCreate, parentId = null }) => {
         toast.error(error.message);
       }
     } else {
-      console.error('onCreate function is not defined');
-      toast.error('Creation function not available');
+      console.error("onCreate function is not defined");
+      toast.error("Creation function not available");
     }
   };
 
@@ -40,9 +40,9 @@ const NewFolderForm = ({ onClose, onCreate, parentId = null }) => {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [onClose]);
 
@@ -61,104 +61,114 @@ const NewFolderForm = ({ onClose, onCreate, parentId = null }) => {
   };
 
   return (
-    <div className='fixed inset-0 flex items-center justify-center p-4 bg-gray-500 bg-opacity-75 transition-opacity duration-300 ease-in-out'>
+    <div className="fixed inset-0 flex items-center justify-center p-4 bg-gray-500 bg-opacity-75 transition-opacity duration-300 ease-in-out z-[9999]">
       <Toaster />
       <div
         ref={modalRef}
-        className='bg-white rounded-md shadow-lg max-w-sm lg:max-w-xl w-full overflow-hidden'
+        className="bg-white rounded-md shadow-lg max-w-sm lg:max-w-xl w-full overflow-hidden"
       >
-        <div className='flex justify-between px-4 py-5 border-b border-gray-200 sm:px-6'>
-          <h3 className='text-lg text-gray-60'>Create New Folder</h3>
-          <CircleButton title={'Close modal'} icon={<IoClose />} onClick={handleCloseModal} />
+        <div className="flex justify-between px-4 py-5 border-b border-gray-200 sm:px-6">
+          <h3 className="text-lg text-gray-60">Create New Folder</h3>
+          <CircleButton
+            title={"Close modal"}
+            icon={<IoClose />}
+            onClick={handleCloseModal}
+          />
         </div>
 
-        <form onSubmit={handleSubmit} className='px-4 py-5 space-y-6 sm:p-6'>
-          <div className='relative w-full min-w-[200px] h-10'>
+        <form onSubmit={handleSubmit} className="px-4 py-5 space-y-6 sm:p-6">
+          <div className="relative w-full min-w-[200px] h-10">
             <input
-              className='peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-gray-400 placeholder-shown:border-t-gray-400 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-gray-400 focus:border-gray-900'
-              type='text'
-              id='folderName'
+              className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-gray-400 placeholder-shown:border-t-gray-400 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-gray-400 focus:border-gray-900"
+              type="text"
+              id="folderName"
               value={folderName}
               onChange={(e) => setFolderName(e.target.value)}
-              placeholder=' '
+              placeholder=" "
               required
             />
             <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-gray-400 peer-focus:before:!border-gray-900 after:border-gray-400 peer-focus:after:!border-gray-900">
               Folder name
             </label>
           </div>
-          <div className='grid grid-cols-2 gap-4'>
-            <div className='flex flex-col'>
-              <label htmlFor='dueDate' className='block text-sm font-medium text-gray-700'>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col">
+              <label
+                htmlFor="dueDate"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Due Date
               </label>
               <input
-                type='date'
-                id='dueDate'
+                type="date"
+                id="dueDate"
                 value={dueDate}
                 onChange={(e) => setDueDate(e.target.value)}
-                className='mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md px-3 py-2'
+                className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md px-3 py-2"
               />
             </div>
-            <div className='flex flex-col'>
-              <label htmlFor='uploadLimit' className='block text-sm font-medium text-gray-700'>
+            <div className="flex flex-col">
+              <label
+                htmlFor="uploadLimit"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Upload Limit
               </label>
-              <div className='relative flex items-center'>
+              <div className="relative flex items-center">
                 <button
-                  type='button'
+                  type="button"
                   onClick={decrementUploadLimit}
-                  className='bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-l-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none'
+                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-l-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
                 >
                   <svg
-                    className='w-3 h-3 text-gray-900 dark:text-white'
-                    aria-hidden='true'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 18 2'
+                    className="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 2"
                   >
                     <path
-                      stroke='currentColor'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       strokeWidth={2}
-                      d='M1 1h16'
+                      d="M1 1h16"
                     />
                   </svg>
                 </button>
                 <input
-                  type='text'
-                  id='uploadLimit'
+                  type="text"
+                  id="uploadLimit"
                   value={uploadLimit}
                   readOnly
-                  className='bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
                 <button
-                  type='button'
+                  type="button"
                   onClick={incrementUploadLimit}
-                  className='bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-r-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none'
+                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-r-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
                 >
                   <svg
-                    className='w-3 h-3 text-gray-900 dark:text-white'
-                    aria-hidden='true'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 18 18'
+                    className="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 18"
                   >
                     <path
-                      stroke='currentColor'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       strokeWidth={2}
-                      d='M9 1v16M1 9h16'
+                      d="M9 1v16M1 9h16"
                     />
                   </svg>
                 </button>
               </div>
             </div>
           </div>
-          <div className='flex justify-end'>
-            <OvalButton text={'Create'} />
+          <div className="flex justify-end">
+            <OvalButton text={"Create"} />
           </div>
         </form>
       </div>
