aac138b8eb01af550c43ad2453996569d5cbb4ca
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-06-08 20:26:21 +0800

    update

--- client/src/pages/users/landing-page.jsx
+++ client/src/pages/users/landing-page.jsx
@@ -1,16 +1,11 @@
-
-import CertifcateOfAuthenticity from "@/components/landing-page/CertofAuthenticity/certofAuthenticitypage";
-import HomePage from "@/components/landing-page/Homepages/homepage.jsx";
-
+import CertifcateOfAuthenticity from '@/components/landing-page/CertofAuthenticity/certofAuthenticitypage';
+import HomePage from '@/components/landing-page/Homepages/homepage.jsx';
 
 export default function LandingPage() {
   return (
     <div>
-     
-        
-   {/* <HomePage/> */}
-   <CertifcateOfAuthenticity/>
-       
+      <HomePage />
+      {/* <CertifcateOfAuthenticity/> */}
     </div>
   );
 }
