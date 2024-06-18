a17b9e7bd45a2f5810ef7d9983416cce4e30dcca
CodeGaia <jasonnazzerocampo@gmail.com>
2024-06-18 16:34:59 +0800

    a

--- client/src/components/landing-page/about/buildingandfacilities/facilitiescontent.jsx
+++ client/src/components/landing-page/about/buildingandfacilities/facilitiescontent.jsx
@@ -1,27 +1,29 @@
 import { bgHeader } from "@/configs/LanfingPageConfigs/bgheader";
-import { data } from "@/configs/LanfingPageConfigs/facilitiespage";
-
+import {
+  building,
+  buildingv2,
+  data,
+  facilities,
+  facilitiesv2,
+} from "@/configs/LanfingPageConfigs/facilitiespage";
 
 export default function FacilitiesContent() {
-    return(
-        <>
-        <section className="bg-gradient-to-r from-[#e9cf5e] to-[#fffbfb]  w-full ">
+  return (
+    <>
+      <section className="bg-gradient-to-r from-[#e9cf5e] to-[#fffbfb]  w-full ">
         <div className=" w-full flex flex-col  justify-center  ">
           <div
             className="w-full text-center select-none bg-no-repeat bg-cover  bg-center shadow-xl "
-            style={{ backgroundImage: `url(${bgHeader.bgheader2})` }}
+            style={{ backgroundImage: `url(${bgHeader.bgheader1})` }}
           >
             <header className="border w-full px-20 lg:px-30 xl:px-40 h-full py-10">
-            <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#dca819]">
+              <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#dca819]">
                 {data.title}
               </h2>
             </header>
           </div>
           <div className="px-20 lg:px-40 py-10">
-            <div
-              className="relative   "
-              style={{ paddingTop: "56.25%" }}
-            >
+            <div className="relative   " style={{ paddingTop: "56.25%" }}>
               {/* 16:9 aspect ratio (h/w * 100) */}
               <iframe
                 src={data.video}
@@ -33,8 +35,171 @@ export default function FacilitiesContent() {
           </div>
         </div>
 
-        </section>
+        <div>
+          <div
+            className="w-full text-center select-none bg-no-repeat bg-cover  bg-center shadow-xl "
+            style={{ backgroundImage: `url(${bgHeader.bgheader2})` }}
+          >
+            <header className=" w-full px-20 lg:px-30 xl:px-40 h-full py-10">
+              <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#dca819]">
+                {data.buildingtitle}
+              </h2>
+            </header>
+          </div>
+          {building.map((items) => (
+            <div key={items.id} className=" ">
+              <div className="w-full pb-10  ">
+                <header className="w-full px-20 lg:px-30 xl:px-40 h-full py-10 flex lg:flex-row flex-col lg:gap-8">
+                  <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#3a3834]">
+                    {items.building}
+                  </h2>
+                  <h2 className="font-bold text-3xl md:text-3xl lg:text-3xl pt-1 text-[#3a3834]">
+                    {items.title}
+                  </h2>
+                </header>
+                <div className=" h-full px-20  flex justify-center ">
+                  <div className=" h-full w-full max-w-screen-lg">
+                    <img
+                      src={items.img}
+                      className="flex object-center z-[10] shadow-lg rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* <div className="border mx-10 mt-10 border-[#3a3834]"></div> */}
+            </div>
+          ))}
 
-        </>
-    );
+          <div className="w-full pb-10  ">
+            <header className="w-full px-20 lg:px-30 xl:px-40 h-full py-10 flex lg:flex-row flex-col lg:gap-8">
+              <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#3a3834]">
+                {data.buildingD}
+              </h2>
+              <h2 className="font-bold text-3xl md:text-3xl lg:text-3xl pt-1 text-[#3a3834]">
+                {data.buildingtitle}
+              </h2>
+            </header>
+            <div className="flex flex-col gap-4">
+              <div className=" h-full px-20 lg:px-40  flex justify-center gap-4 ">
+                <div className=" flex lg:flex-row h-full w-full max-w-screen-lg">
+                  <img
+                    src={data.buildingDimg1}
+                    className="h-auto object-center z-[10] shadow-lg rounded"
+                  />
+                </div>
+                <div className="  w-[48%] h-auto max-w-screen-lg">
+                  <img
+                    src={data.buildingDimg2}
+                    className="flex object-center z-[10] shadow-lg rounded"
+                  />
+                </div>
+              </div>
+
+              <div className=" h-full px-20 lg:px-40  flex justify-center gap-4 flex-row-reverse">
+                <div className=" flex lg:flex-row h-full w-full max-w-screen-lg">
+                  <img
+                    src={data.buildingDimg4}
+                    className="h-auto object-center z-[10] shadow-lg rounded"
+                  />
+                </div>
+                <div className=" w-[48%] h-auto max-w-screen-lg">
+                  <img
+                    src={data.buildingDimg3}
+                    className="flex object-center z-[10] shadow-lg rounded"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {buildingv2.map((items) => (
+              <div key={items.id} className=" ">
+                <div className="w-full pb-10  ">
+                  <header className="w-full px-20 lg:px-30 xl:px-40 h-full py-10 flex lg:flex-row flex-col lg:gap-8">
+                    <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#3a3834]">
+                      {items.building}
+                    </h2>
+                    <h2 className="font-bold text-3xl md:text-3xl lg:text-3xl pt-1 text-[#3a3834]">
+                      {items.title}
+                    </h2>
+                  </header>
+                  <div className=" h-full px-20  flex justify-center ">
+                    <div className=" h-full w-full max-w-screen-lg">
+                      <img
+                        src={items.img}
+                        className="flex object-center z-[10] shadow-lg rounded"
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* <div className="border mx-10 mt-10 border-[#3a3834]"></div> */}
+              </div>
+            ))}
+
+            <div
+              className="w-full text-center select-none bg-no-repeat bg-cover  bg-center shadow-xl "
+              style={{ backgroundImage: `url(${bgHeader.bgheader2})` }}
+            >
+              <header className=" w-full px-20 lg:px-30 xl:px-40 h-full py-10">
+                <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl text-[#dca819]">
+                  {data.facilititestitle}
+                </h2>
+              </header>
+            </div>
+
+            {facilities.map((items) => (
+              <div key={items.id} className=" ">
+                <div className="w-full pb-10  ">
+                  <header className="w-full px-20 lg:px-30 xl:px-40 h-full py-10 lg:gap-8">
+                    <h2 className="font-bold text-3xl md:text-3xl lg:text-5xl pt-1 text-[#3a3834]">
+                      {items.title}
+                    </h2>
+                  </header>
+                  <div className=" h-full px-20  flex justify-center ">
+                    <div className=" h-full w-full max-w-screen-lg">
+                      <img
+                        src={items.img}
+                        className="flex object-center z-[10] shadow-lg rounded"
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* <div className="border mx-10 mt-10 border-[#3a3834]"></div> */}
+              </div>
+            ))}
+
+            {facilitiesv2.map((items) => (
+              <div key={items.id} className=" ">
+                <div className="w-full pb-10  ">
+                  <header className="w-full px-20 lg:px-30 xl:px-40 h-full py-10 lg:gap-8">
+                    <h2 className="font-bold text-3xl md:text-3xl lg:text-5xl pt-1 text-[#3a3834]">
+                      {items.title}
+                    </h2>
+                  </header>
+                  <div className=" h-full px-20 lg:px-40  flex justify-center gap-4 ">
+                    <div className=" flex lg:flex-row h-full w-full max-w-screen-lg">
+                      <img
+                        src={items.img1}
+                        className="h-auto object-center z-[10] shadow-lg rounded"
+                      />
+                    </div>
+                    <div className=" flex flex-col w-[48%]  max-w-screen-lg gap-4">
+                      <img
+                        src={items.img2}
+                        className="flex object-center z-[10] shadow-lg rounded h-[50%]"
+                      />
+                      <img
+                        src={items.img3}
+                        className="flex object-center z-[10] shadow-lg rounded h-[50%]"
+                      />
+                    </div>
+                  </div>
+                </div>
+                {/* <div className="border mx-10 mt-10 border-[#3a3834]"></div> */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- client/src/configs/LanfingPageConfigs/facilitiespage.js
+++ client/src/configs/LanfingPageConfigs/facilitiespage.js
@@ -1,3 +1,21 @@
+import buildingA from "@/assets/facilities/BuildingA.webp"
+import buildingB from "@/assets/facilities/BuildingB.webp"
+import buildingC from "@/assets/facilities/BuildingC.webp"
+import buildingD1 from "@/assets/facilities/BuildingD1.webp"
+import buildingD2 from "@/assets/facilities/BuildingD2.webp"
+import buildingD3 from "@/assets/facilities/BuildingD3.webp"
+import buildingD4 from "@/assets/facilities/BuildingD4.webp"
+import buildingE from "@/assets/facilities/BuildingE.webp"
+import buildingF from "@/assets/facilities/BuildingF.webp"
+import buildingG from "@/assets/facilities/BuildingG.webp"
+import ICT from "@/assets/facilities/ICT1-2.webp"
+import GYM1 from  "@/assets/facilities/Gym1.webp"
+import GYM2 from  "@/assets/facilities/Gym2.webp"
+import GYM3 from  "@/assets/facilities/Gym3.webp"
+import Lib1 from "@/assets/facilities/Library1.webp"
+import Lib2 from "@/assets/facilities/Library2.webp"
+import Lib3 from "@/assets/facilities/Library3.webp"
+
 export const data = {
   //header
   building: "Buildings",
@@ -7,5 +25,85 @@ export const data = {
 
 //content
 title:"Virtual Tour",
-video: "https://drive.google.com/file/d/1RsomBFjQeCaOCQ4BI3I-8s4b7gwXhEtz/preview",
+video: "https://drive.google.com/file/d/1SpnWFVZ_P1FA-1fIxN6sztoJFgOXlUq4/preview",
+
+buildingtitle: "Buildings",
+facilititestitle: "Facilitites",
+
+buildingD: "BUILDING D: ",
+titleD: "Nantes Building",
+buildingDimg1: buildingD1,
+buildingDimg2: buildingD2,
+buildingDimg3: buildingD3,
+buildingDimg4: buildingD4,
 };
+
+
+export const building = [
+  {
+    id: 1,
+    building:"BUILDING A: ",
+    title: " Don Gregorio Yumul Sr. Memorial Bldg  ",
+    img: buildingA,
+  },
+  {
+    id: 2,
+    building:"BUILDING B: ",
+    title: " Administration Building   ",
+    img: buildingB,
+  },
+  {
+    id: 3,
+    building:"BUILDING C: ",
+    title: " Aristedes Yumul Sr. Building dministration Building   ",
+    img: buildingC,
+  },
+
+]
+
+export const buildingv2 = [
+  {
+    id: 1,
+    building:"BUILDING E: ",
+    title: " Row Building  ",
+    img: buildingE,
+  },
+  {
+    id: 2,
+    building:"BUILDING F: ",
+    title: "Cortuna Building    ",
+    img: buildingF,
+  },
+  {
+    id: 3,
+    building:"BUILDING G: ",
+    title: " STAN Building   ",
+    img: buildingG,
+  },
+
+]
+
+export const facilities = [
+  {
+    id: 1,
+    title: " ICT- LABORATORY- 1 & 2",
+    img: ICT,
+  },
+]
+
+export const facilitiesv2 = [
+  {
+    id: 1,
+    title: "GYMNASIUM ",
+    img1: GYM1,
+    img2: GYM2,
+    img3: GYM3,
+  },
+  {
+    id: 2,
+    title:"LIBRARY ",
+    img1: Lib1,
+    img2: Lib2,
+    img3: Lib3,
+  },
+]
