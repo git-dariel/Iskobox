b4ba0c7569d0cccd29021996f863c808913e0307
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-06-23 15:41:00 +0800

    big update in app

--- client/src/configs/constant.js
+++ client/src/configs/constant.js
@@ -1,13 +1,22 @@
 export const constants = {
+  // FIREBASE: {
+  //   API_KEY: 'AIzaSyCFyEQKxn5m2BHuLa1Dffkd-h5Y1Io-yfA',
+  //   AUTH_DOMAIN: 'sharehub-245d9.firebaseapp.com',
+  //   PROJECT_ID: 'sharehub-245d9',
+  //   STORAGE_BUCKET: 'sharehub-245d9.appspot.com',
+  //   MESSAGING_SENDER_ID: '535458723446',
+  //   APP_ID: '1:535458723446:web:54d3a2777d9bef18e69157',
+  // },
+
   FIREBASE: {
-    API_KEY: 'AIzaSyCFyEQKxn5m2BHuLa1Dffkd-h5Y1Io-yfA',
-    AUTH_DOMAIN: 'sharehub-245d9.firebaseapp.com',
-    PROJECT_ID: 'sharehub-245d9',
-    STORAGE_BUCKET: 'sharehub-245d9.appspot.com',
-    MESSAGING_SENDER_ID: '535458723446',
-    APP_ID: '1:535458723446:web:54d3a2777d9bef18e69157',
+    API_KEY: "AIzaSyCEpvI4I7YRTg_4I7VP-k5SCG5a9UOmptU",
+    AUTH_DOMAIN: "iskobox.firebaseapp.com",
+    PROJECT_ID: "iskobox",
+    STORAGE_BUCKET: "iskobox.appspot.com",
+    MESSAGING_SENDER_ID: "254769554339",
+    APP_ID: "1:254769554339:web:92052bb254efabffd4b016",
   },
 
-  PUP_NAME: ' POLYTECHNIC UNIVERSITY OF THE PHILIPPINES',
-  BRANCH: 'LOPEZ BRANCH',
+  PUP_NAME: " POLYTECHNIC UNIVERSITY OF THE PHILIPPINES",
+  BRANCH: "LOPEZ BRANCH",
 };
