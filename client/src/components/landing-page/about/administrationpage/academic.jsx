bb10ae24b09af3b05f978862f47287d0449a7cb9
CodeGaia <jasonnazzerocampo@gmail.com>
2024-07-02 19:38:11 +0800

    updated

--- client/src/components/landing-page/about/administrationpage/academic.jsx
+++ client/src/components/landing-page/about/administrationpage/academic.jsx
@@ -6,18 +6,20 @@ import { bgHeader } from "@/configs/LanfingPageConfigs/bgheader";
 
 export default function Academic() {
   return (
-    <section className="bg-gradient-to-r from-[#e9cf5e] to-[#fffbfb] ">
+    <section className="bg-gradient-to-r from-[#e9cf5e] to-[#fffbfb]">
+      {/* Header Section */}
       <div
-        className="w-full h-auto text-center select-none bg-cover bg-no-repeat bg-center shadow-xl " 
+        className="w-full h-auto text-center select-none bg-cover bg-no-repeat bg-center shadow-xl"
         style={{ backgroundImage: `url(${bgHeader.bgheader2})` }}
       >
-        <div className="">
-          <h1 className="text-[#dca819] text-lg font-extrabold md:text-3xl px-10 py-10 flex flex-col">
+        <div>
+          <h1 className="text-[#334747] text-lg font-extrabold md:text-3xl px-10 py-10 flex flex-col">
             {administration.academic}
           </h1>
         </div>
       </div>
 
+      {/* Head of Academic Section */}
       <div className="w-full h-auto flex flex-col gap-8 lg:gap-0 lg:flex-row py-10 justify-center">
         <div className="flex flex-col items-center gap-4 w-full lg:w-[40%] h-full py-4">
           <div className="flex justify-center">
@@ -36,12 +38,15 @@ export default function Academic() {
         </div>
       </div>
 
+      {/* Academic Staff Section */}
       <div className="w-full px-10 py-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {academic.map((item, index) => (
           <div
             key={item.id}
             className={`flex flex-col items-center gap-4 py-4 ${
-              index >= 4 ? "col-span-2 justify-self-center" : ""
+              index >= academic.length - 2
+                ? "sm:col-span-2 sm:justify-self-center"
+                : ""
             }`}
           >
             <div className="flex justify-center">
--- client/src/components/landing-page/about/administrationpage/designees.jsx
+++ client/src/components/landing-page/about/administrationpage/designees.jsx
@@ -9,7 +9,7 @@ export default function Designees() {
         style={{ backgroundImage: `url(${bgHeader.bgheader2})` }}
       >
         <div className="">
-          <h1 className="text-[#dca819] text-lg font-extrabold md:text-3xl px-10 py-10 flex flex-col">
+          <h1 className="text-[#334747] text-lg font-extrabold md:text-3xl px-10 py-10 flex flex-col">
             {administration.designees}
           </h1>
         </div>
@@ -24,16 +24,16 @@ export default function Designees() {
               className="w-[300px] h-auto rounded-md shadow-lg"
             />
           </div>
-          <div className="text-[#810101] flex justify-center items-center flex-col font-bold text-xl">
+          <div className="text-[#810101] flex justify-center items-center flex-col font-bold text-xl text-center">
             <span className="text-md">{administration.directorname}</span>
             <span className="text-sm">{administration.directorposition}</span>
           </div>
         </div>
       </div>
       
-    <div className="flex flex-row px-10">
+    <div className="flex flex-col lg:flex-row justify-center items-center  px-10">
       {designees.map((item) => (
-        <div key={item.id} className=" w-[25%] justify-center items-center h-full flex flex-row gap-8 lg:gap-0  py-10">
+        <div key={item.id} className=" w-full  justify-center items-center h-full flex flex-row gap-10 lg:gap-0  py-10 text-center">
           <div className="flex flex-col items-center gap-4  py-4 ">
             <div className="flex justify-center">
               <img
