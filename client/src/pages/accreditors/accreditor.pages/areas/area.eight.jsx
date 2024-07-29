606176ca977eaed670c18b27910e7056917e0517
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-07-29 08:13:30 +0800

    feat(area_eight): add ppp

--- client/src/pages/accreditors/accreditor.pages/areas/area.eight.jsx
+++ client/src/pages/accreditors/accreditor.pages/areas/area.eight.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Paperclip, Fullscreen} from "lucide-react";
+import { Paperclip, Fullscreen } from "lucide-react";
 import { FaRegWindowClose } from "react-icons/fa";
 import common from "@/configs/common.config";
 import MainLayout from "../../layout/main.layout";
@@ -29,7 +29,7 @@ const AreaEight = () => {
 
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
-  const [name, setName] = useState(null)
+  const [name, setName] = useState(null);
   const iframeRef = useRef(null);
 
   const slideInKeyframes = `
@@ -77,9 +77,7 @@ const AreaEight = () => {
       if (fileType === "pdf") {
         contentUrl = url;
       } else {
-        contentUrl = `https://docs.google.com/gview?url=${encodeURIComponent(
-          url
-        )}&embedded=true`;
+        contentUrl = `https://docs.google.com/gview?url=${encodeURIComponent(url)}&embedded=true`;
       }
       setName(name);
       setSelectedFile(contentUrl);
@@ -181,12 +179,11 @@ const AreaEight = () => {
               </header>
             </div>
             <div className="md:w-[100vh] md:h-[90vh] w-[40vh] h-[50vh] flex items-center justify-center bg-transparent my-2">
-              <h1>No files yet</h1>
-              {/* <iframe
+              <iframe
                 src={embedLinkPPP}
                 className="w-full h-full border-none"
                 allow="autoplay"
-              ></iframe> */}
+              ></iframe>
             </div>
           </div>
         </section>
@@ -387,9 +384,7 @@ const AreaEight = () => {
           <div
             className="fixed inset-y-0 right-0 z-50 flex items-center shadow-2xl w-1/2 bg-black bg-opacity-0 transition-opacity duration-5000 ease-in-out"
             style={{
-              animation: `${
-                modalOpen ? "slideIn 2s forwards" : "slideOut 2s forwards"
-              }`,
+              animation: `${modalOpen ? "slideIn 2s forwards" : "slideOut 2s forwards"}`,
             }}
           >
             <div className="bg-white w-full h-full p-4 overflow-y-auto">
